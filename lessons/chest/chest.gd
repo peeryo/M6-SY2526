bdac9f890910611b2a44f8d833d2783420eff7b6
[peeryo] <peeryo@sfusd.edu>
2026-01-19 16:55:21 -0800

    M6L5 completed

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -1,5 +1,10 @@
 extends Area2D
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func open() -> void:
+	animation_player.play("open")
+	input_pickable = false
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
@@ -17,3 +22,12 @@ func _on_mouse_exited():
 func set_outline_thickness(new_thickness: float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
 	
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.is_pressed()
+	)		
+	
+	if event_is_mouse_click:
+		open()
