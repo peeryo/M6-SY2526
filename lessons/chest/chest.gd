23c43c5441da8757a13be346ddf6b983c78ef6d1
Owen Peery <peeryo@sfusd.edu>
2026-01-29 12:24:34 -0800

    M6L3 completed

---
+++ lessons/chest/chest.gd
@@ -0,0 +1,19 @@
+extends Area2D
+@onready var canvas_group: CanvasGroup = $CanvasGroup
+
+
+func _ready() -> void:
+	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	
+func set_outline_thickness(new_thickness: float) -> void:
+		canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+	
+func _on_mouse_entered():
+	var tween := create_tween()
+	tween.tween_method(set_outline_thickness, 3.0, 10.0, 0.08)
+	
+func _on_mouse_exited():
+	var tween := create_tween()
+	tween.tween_method(set_outline_thickness, 10.0, 3.0, 0.08)
