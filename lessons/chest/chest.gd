c41e8b894a5ec41d5020f25ca168b62147f63810
Owen Peery <peeryo@sfusd.edu>
2026-02-03 11:50:23 -0800

    M6 L4 and L5 completed

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -1,19 +1,36 @@
 extends Area2D
-@onready var canvas_group: CanvasGroup = $CanvasGroup
 
+
+@onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
-	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
-	
+	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
+
 func set_outline_thickness(new_thickness: float) -> void:
-		canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
-	
-func _on_mouse_entered():
+	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+
+func _on_mouse_entered() -> void:
 	var tween := create_tween()
-	tween.tween_method(set_outline_thickness, 3.0, 10.0, 0.08)
-	
-func _on_mouse_exited():
+	tween.tween_method(set_outline_thickness, 3.0, 6.0, 0.08)
+
+func _on_mouse_exited() -> void:
 	var tween := create_tween()
-	tween.tween_method(set_outline_thickness, 10.0, 3.0, 0.08)
+	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
+
+func open() -> void:
+	animation_player.play("open")
+
+	input_pickable = false
