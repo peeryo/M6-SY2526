d6dce55f9dc778e706e069b56d945399b297b946
Owen Peery <peeryo@sfusd.edu>
2026-02-03 14:33:27 -0800

    M6 L4 and L5 completed

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -1,6 +1,22 @@
 extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func open() -> void:
+	animation_player.play("open")
+	
+	input_pickable = false
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_index: int) -> void:
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and 
+		event.is_pressed()
+	)
+	
+	if event_is_mouse_click:
+		open()
 
 func _ready() -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", 3.0)
